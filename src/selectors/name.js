851b92ecff56eeb97eba9b10eb7df1f34c5aa756
dcuthell <dcc1447@gmail.com>
2018-01-12 10:19:00 -0800

    add edits

--- src/selectors/name.js
+++ src/selectors/name.js
@@ -1,1 +1,5 @@
 export default state => state.name.get('name');
+
+/**************************************************************
+Here we have an example of a selector. It's very basic.
+**************************************************************/
